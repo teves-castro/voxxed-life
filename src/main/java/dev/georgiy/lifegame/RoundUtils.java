ffa297a776ad96d9bb530e7ab08d6d28aed0d637
Giorgos <mayshmaz@gmail.com>
2019-06-21 12:48:49 +0300

    refactored: changed points clearing function to use stream, removed ifs

--- src/main/java/dev/georgiy/lifegame/RoundUtils.java
+++ src/main/java/dev/georgiy/lifegame/RoundUtils.java
@@ -2,30 +2,39 @@ package dev.georgiy.lifegame;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class RoundUtils {
     public static Set<Point> getNextGeneration(Set<Point> pointsSeeded) {
-        Set<Point> newGenPoints = removeDiedPoints(pointsSeeded);
+        Set<Point> newGenPoints = calculateDiedPoints(pointsSeeded);
+        newGenPoints.addAll(calculateBornPoints(pointsSeeded));
+        return  newGenPoints;
+    }
 
+    private static Set<Point> calculateDiedPoints(Set<Point> pointsSeeded) {
+
+        return pointsSeeded.stream()
+                .filter(p -> p.countNeighborsInSet(pointsSeeded)>=2
+                        && p.countNeighborsInSet(pointsSeeded)<=3)
+                .collect(Collectors.toSet());
+
+    }
+
+    private static Set<Point> calculateBornPoints(Set<Point> pointsSeeded) {
+        Set<Point> newGenPoints = new HashSet<>();
         Set<Point> deadNeighbors = new HashSet<>();
         pointsSeeded.forEach(p -> deadNeighbors.addAll(p.getNeighbourPoints()));
         deadNeighbors.removeAll(pointsSeeded);
         deadNeighbors.forEach(point -> {
             int count = point.countNeighborsInSet(pointsSeeded);
-            if(count==3){ newGenPoints.add(point); }
-        });
-        return  newGenPoints;
-    }
-
-    private static Set<Point> removeDiedPoints(Set<Point> pointsSeeded) {
-        Set<Point> newGenPoints = new HashSet<>();
-        pointsSeeded.forEach(point -> {
-            int count = point.countNeighborsInSet(pointsSeeded);
-            if(count>=2 && count<=3){ newGenPoints.add(point); }
+            if (count == 3) {
+                newGenPoints.add(point);
+            }
             //todo remove ifs
         });
         return newGenPoints;
     }
+
 
 
 }
